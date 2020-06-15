af0dd36496da64670adbb271583d38f3e746df3a
joanicacardoso <joana.cardoso.reis@gmail.com>
2020-06-15 20:07:00 +0100

    Implemented JS Open Weather API
    It's still missing the units conversion

---
+++ src/app.js
@@ -0,0 +1,167 @@
+// Set local time
+function formatDate(timestamp) {
+  let currentdate = new Date(timestamp);
+  let date = currentdate.getDate();
+  let hours = currentdate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentdate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentdate.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[currentdate.getMonth()];
+  return `${day}, ${month} ${date}</br>${hours}:${minutes}`;
+}
+
+//Search Engine
+
+function showTemperature(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  celsiusTemp = response.data.main.temp;
+  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemp);
+  savedTemperature = Math.round(response.data.main.temp);
+  document.querySelector("#current-condition").innerHTML =
+    response.data.weather[0].main;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#date").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
+  /*
+  let icon = document.querySelector("#weather-icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );*/
+}
+
+function formatHours(timestamp) {
+  let currentdate = new Date(timestamp);
+  let hours = currentdate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentdate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 5; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `<div class="col">
+                    <p>${formatHours(forecast.dt * 1000)}</p>
+                    <svg width="30" height="30" viewBox="0 0 115 115" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="57.5" cy="57.5" r="57.5" fill="#FFCA0F" />
+                    </svg>
+                    <p class="forecast-temp">${Math.round(
+                      forecast.main.temp_min
+                    )}º <strong>${Math.round(
+      forecast.main.temp_max
+    )}º</strong></p>
+                </div>`;
+  }
+}
+
+function search(city) {
+  let apiKey = "48f466c2eeef74f8d4b3c29e67806457";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  search(city);
+}
+
+let cityForm = document.querySelector("#search-form");
+cityForm.addEventListener("submit", searchCity);
+
+let savedTemperature = 0;
+
+/*Current location
+
+let buttonCurrentLocation = document.querySelector("#current-location-btn");
+
+function retrievePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "48f466c2eeef74f8d4b3c29e67806457";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+buttonCurrentLocation.addEventListener("click", getPosition);
+
+// Change temperature units - WIP
+
+function convertCel(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#temperature");
+  let units = document.querySelector("#units");
+  currentTemp.innerHTML = Math.round(celsiusTemp);
+  units.innerHTML = ` ºC`;
+}
+
+function convertFah(event) {
+  event.preventDefault();
+  let currentTemp = document.querySelector("#temperature");
+  let fTemp = (celsiusTemp * 9) / 5 + 32;
+  currentTemp.innerHTML = Math.round(fTemp);
+  let units = document.querySelector("#units");
+  units.innerHTML = ` ºF`;
+  //add active class from celsius
+  // celsiusTemp.classList.add(".active");
+}
+
+let celsiusTemp = null;
+
+let fahrButton = document.querySelector("#fahr");
+fahrButton.addEventListener("click", convertFah);
+
+let celsiusButton = document.querySelector("#celsius");
+celsiusButton.addEventListener("click", convertCel);
+*/
+search("Lisbon");
